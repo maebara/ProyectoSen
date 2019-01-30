3021f6f74ac0a5abdb72cd9ceb129a7d3401f700
Sergio Puca <guitarra_ampli_98@hotmail.com>
2019-01-30 20:04:28 -0300

    More points, looks better

--- src/edu/sergio/graficos/JPanelGrafico.java
+++ src/edu/sergio/graficos/JPanelGrafico.java
@@ -5,21 +5,26 @@ import javax.swing.JPanel;
 
 public class JPanelGrafico extends JPanel {
 	private double[] arrX;
-
+	private double amplitud;
+	
 	public JPanelGrafico() {
-		this.arrX = new double[19];
+		this.arrX = new double[40];
+		double val = 0.1;
 		for (int i = 0; i < arrX.length; i++) {
-			arrX[i] = i;
+			arrX[i] = val;
+			val = val + 0.1;
 		}
+		amplitud = 100;
 	}
 
 	// in another graphics frameworks this method works like update().
 	public void paintComponent(Graphics g) {
-	
+		
 		for (int i = 0; i < arrX.length; i++) {
-			g.fillOval(i * 40, (int)(Math.sin(arrX[i]) * 100) + getHeight()/2, 10, 10);
-			arrX[i] = arrX[i] + 0.09;
+			g.fillOval(i * 14, (int)(Math.sin(arrX[i]) * amplitud) + getHeight()/2, 10, 10);
+			arrX[i] = arrX[i] + 0.1;
 		}
+		
 	}
 
 }
