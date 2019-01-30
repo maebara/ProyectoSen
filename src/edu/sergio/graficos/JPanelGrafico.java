1e0ecfa6467391819c1f85ee238e81c41e0541bd
Sergio Puca <guitarra_ampli_98@hotmail.com>
2019-01-30 19:06:07 -0300

    One Point Dancing
    just one point doing dancing the sin function

--- src/edu/sergio/graficos/JPanelGrafico.java
+++ src/edu/sergio/graficos/JPanelGrafico.java
@@ -1,17 +1,29 @@
 package edu.sergio.graficos;
 
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 
-public class JPanelGrafico extends JPanel{
+public class JPanelGrafico extends JPanel {
+
+	private Punto punto;
+
+	public JPanelGrafico() {
+		this.punto = new Punto(0, 0);
+	}
 
 	public void paintComponent(Graphics g) {
-		g.drawLine(1, 1, 10, 10);
+		int y = (int) (punto.getY() * 100);
+//		for (int i = 10; i < 100; i = i + 10) {
+//			g.fillOval(i, y + 200 - i, 10, 10);
+//		}
+		g.fillOval(10, y + 200, 10, 10);
+	}	
+
+	public Punto getPunto() {
+		return punto;
 	}
-	
-	 
-	
-	
-	
+
+	public void setPunto(Punto punto) {
+		this.punto = punto;
+	}
 }
--- src/edu/sergio/graficos/JVentana.java
+++ src/edu/sergio/graficos/JVentana.java
@@ -1,31 +1,51 @@
 package edu.sergio.graficos;
 
 import java.awt.Color;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.WindowConstants;
 
 public class JVentana extends JFrame{
-
 	private JPanelGrafico panelGrafico;
+	private double x;
 	
 	public JVentana() {
 		super();
-		this.setBounds(0, 0, 400, 400);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		
-		this.panelGrafico = new JPanelGrafico();
-		this.setContentPane(panelGrafico);
-		this.setBackground(Color.WHITE);
+		setBounds(0, 0, 600, 400);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setResizable(false);
+		panelGrafico = new JPanelGrafico();
+		setContentPane(panelGrafico);
+		setBackground(Color.WHITE);
+		x = 0;		
 	}
-	
-	
-	
+		
+	public void iniciarAnimacion() {
+		Timer timer = new Timer(17, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				update();
+			}
+		});
+		
+		timer.start();
+	}
+
+	private void update() {
+		double senX = Math.sin(this.x);
+		Punto p = panelGrafico.getPunto();
+		p.setX(x);
+		p.setY(senX);
+		x = x + 0.09;
+		repaint();
+	}
+
 	public static void main(String[] args) {
 		JVentana ventana = new JVentana();
 		ventana.setVisible(true);
+		ventana.iniciarAnimacion();
 	}
 }
